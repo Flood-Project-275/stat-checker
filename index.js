e934fb29db28e53cc7533be787bc284b526f0ab5
littleBitsman <adittyanischal@hotmail.com>
2023-05-01 17:51:28 -0400

    V1.0.0 finished

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-async function FetchLeaderboard() {
-    const f = await fetch('http://moonsteal.eastus.cloudapp.azure.com:8080/stats/asteroids/leaderboard', {
-        method: 'GET'
-    }).then((res) => res.json())
-    return f
-}
 async function LookupUser(username) {
     const robloxData = await fetch(`http://moonsteal.eastus.cloudapp.azure.com:8080/roblox-redirect/users/lookup/byUsername?username=${username}`, {
         method: 'GET',
-    }).then((res) => res.json())
+    }).then((res) => res.json()).catch(() => [])
     if (robloxData.length < 1) return false
     const userId = robloxData['id']
     const lookupResponse = (await fetch(`http://moonsteal.eastus.cloudapp.azure.com:8080/stats/lookup?userId=${userId}`, {
@@ -16,3 +10,68 @@ async function LookupUser(username) {
     console.log(lookupResponse)
     return lookupResponse
 }
+async function DoLookup() {
+    const username = document.getElementById('lookup-username-input').value
+    document.getElementById('lookup:username').innerHTML = 'Username: ' + username
+    document.getElementById('lookup:asteroid-count').innerHTML = 'Asteroids: Loading...'
+    const response = await LookupUser(username)
+    if (response == false) {
+        document.getElementById('username-input-error').innerHTML = 'Error: Invalid username!'
+        document.getElementById('lookup:asteroid-count').innerHTML = 'Asteroids: Error!'
+        setTimeout(() => {
+            document.getElementById('username-input-error').innerHTML = ''
+            document.getElementById('lookup:username').innerHTML = 'Username: ???'
+            document.getElementById('lookup:asteroid-count').innerHTML = 'Asteroids: ???'
+        }, 3000)
+        return
+    }
+    const stats = response.stats
+    const asteroids = stats.asteroids
+    document.getElementById('lookup:asteroid-count').innerHTML = 'Asteroids: ' + asteroids.toString()
+}
+
+async function FetchLeaderboard() {
+    const f = await fetch(`http://moonsteal.eastus.cloudapp.azure.com:8080/stats/asteroids/leaderboard`, {
+        method: 'GET'
+    }).then((res) => res.json())
+    f.sort((a, b) => a.place - b.place)
+    console.log(f)
+    return f
+}
+async function ShowLeaderboard() {
+    const leaderboardData = await FetchLeaderboard(true)
+    const Table = document.getElementById('aaaaaaaaaaaaaaaaaaaaaa')
+    for (let index = 0; index < Table.childNodes.length; index++) {
+        const element = Table.children.item(index)
+        element.remove()
+    }
+    const RankHeader = document.createElement('th')
+    RankHeader.innerHTML = 'Rank'
+    const NameHeader = document.createElement('th')
+    NameHeader.innerHTML = 'Username'
+    const AsteroidsHeader = document.createElement('th')
+    AsteroidsHeader.innerHTML = 'Asteroids'
+
+    Table.appendChild(RankHeader)
+    Table.appendChild(NameHeader)
+    Table.appendChild(AsteroidsHeader)
+
+    leaderboardData.forEach(element => {
+        const Row = document.createElement('tr')
+        const Rank = document.createElement('td')
+        const Name = document.createElement('td')
+        const Asteroids = document.createElement('td')
+        Rank.innerHTML = element.place
+        if (element.name != element.displayName) {
+            Name.innerHTML = `${element.displayName} (@${element.name})`
+        } else {
+            Name.innerHTML = element.name
+        }
+        Asteroids.innerHTML = element.asteroids
+        Row.appendChild(Rank)
+        Row.appendChild(Name)
+        Row.appendChild(Asteroids)
+        Table.appendChild(Row)
+    });
+}
+ShowLeaderboard()
